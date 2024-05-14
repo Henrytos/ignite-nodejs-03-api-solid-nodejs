824acdbbc526054bdd5e130c8d564a344ce71a5b
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-05-13 22:50:43 -0300

    adicionando modulo env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { app } from "./app";
-
+import { env } from "./env";
 
 app.listen({
-  host: '0.0.0.0',
-  port: 3333
+  port: env.PORT,
+  host:'0.0.0.0'
 }).then(() => {
   console.log('Server is running ')
 })
