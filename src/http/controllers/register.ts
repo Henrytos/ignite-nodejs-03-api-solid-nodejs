1a275ba08dccb401ca729510282d06173839238f
henryFranz <166771474+PessoasDesaparecidas@users.noreply.github.com>
2024-05-17 16:06:48 -0300

    🚀🚀🚀🚀kksksksks

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,4 @@
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExitsError } from '@/use-cases/errors/user-already-exits-error'
 import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
