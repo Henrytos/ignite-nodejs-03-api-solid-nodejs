bd766e416635b962a77ad35fa10f78f4c3422178
Henry <franzhenry46@gmail.com>
2024-05-18 11:58:50 -0300

    test:criando regra de negocio da validação de check in até 100m da academia

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,13 +1,27 @@
 import { afterEach, beforeEach, describe,  expect,  it , vi } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { CheckInUseCase } from "./check-in";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { Decimal } from "@prisma/client/runtime/library";
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
+
 describe('testing the check in use case',()=>{
     beforeEach(()=>{
         checkInsRepository = new InMemoryCheckInsRepository()
-        sut = new CheckInUseCase(checkInsRepository)
+        gymsRepository = new InMemoryGymsRepository()
+        sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+        gymsRepository.items.push({
+            id:'gym-01',
+            title:'liceu',
+            phone:'',
+            description:'',
+            latitude: new Decimal(0),
+            longitude: new Decimal(0),
+        })
         vi.useFakeTimers()
     })  
 
@@ -16,24 +30,24 @@ describe('testing the check in use case',()=>{
     })
 
     it('should be possible to create a check in', async()=>{
-        const {checkIn} = await sut.execute({gymId:'gym-01',userId:'user-01'} )
+        const {checkIn} = await sut.execute({gymId:'gym-01',userId:'user-01',userLatitude:0,userLongitude:0} )
         expect(checkIn.id).toEqual(expect.any(String))
     })  
 
 
     it('should not be possible not to create two check ins on the same day', async()=>{
         vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
-        await sut.execute({gymId:'gym-01',userId:'user-01'} )
-        await expect(sut.execute({gymId:'gym-01',userId:'user-01'} )
+        await sut.execute({gymId:'gym-01',userId:'user-01', userLatitude:0,userLongitude:0} )
+        await expect(sut.execute({gymId:'gym-01',userId:'user-01', userLatitude:0,userLongitude:0} )
         ).rejects.toBeInstanceOf(Error)
     })  
 
     it('should be possible to create two check ins on different days ', async()=>{
         vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
-        await sut.execute({gymId:'gym-01',userId:'user-01'} )
+        await sut.execute({gymId:'gym-01',userId:'user-01', userLatitude:0,userLongitude:0} )
 
         vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
-        const {checkIn} = await sut.execute({gymId:'gym-01',userId:'user-01'} )
+        const {checkIn} = await sut.execute({gymId:'gym-01',userId:'user-01', userLatitude:0,userLongitude:0} )
         expect(checkIn.id).toEqual(expect.any(String))
     })  
 })
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,19 +1,30 @@
 import { CheckIn, Prisma } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface CheckInUseCaseRequest{
     userId: string;
     gymId: string
+    userLatitude:number
+    userLongitude:number
 }
 interface CheckInUseCaseResponse{
     checkIn:Prisma.CheckInUncheckedCreateInput
 }
 
 export class CheckInUseCase{
-    constructor(private checkInsRepository:CheckInsRepository){}
-    async execute({ gymId , userId }: CheckInUseCaseRequest):Promise<CheckInUseCaseResponse>{
+    constructor(private checkInsRepository:CheckInsRepository, private gymsRepository:GymsRepository){}
+    async execute({ gymId , userId, userLatitude , userLongitude }: CheckInUseCaseRequest):Promise<CheckInUseCaseResponse>{
+        
         const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(userId,new Date())
         
+        const gym = await this.gymsRepository.findById(gymId)
+        // TODO: fazer calculo de distancia entre usuario e academia 
+        if(!gym){
+            throw new ResourceNotFoundError()
+        }
+
         if(checkInOnSameDate){
             throw new Error('error')
         }
@@ -23,6 +34,7 @@ export class CheckInUseCase{
             gym_id: gymId,
             created_at: new Date()
         })
+
         return { checkIn }   
     }
 }
