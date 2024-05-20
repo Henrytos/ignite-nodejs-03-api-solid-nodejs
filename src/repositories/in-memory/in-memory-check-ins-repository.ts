9cf242126caae228386cc23823000e49e6fa9630
Henry <franzhenry46@gmail.com>
2024-05-20 19:07:29 -0300

    feat:implementando check in repository no prisma

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -4,63 +4,63 @@ import { randomUUID } from "crypto";
 import dayjs from "dayjs";
 
 
-export class InMemoryCheckInsRepository implements CheckInsRepository  {
-    async findById(id: string){
-        const checkIn = this.items.find(checkIn=>checkIn.id === id);
-        if(!checkIn){
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+    async findById(id: string) {
+        const checkIn = this.items.find(checkIn => checkIn.id === id);
+        if (!checkIn) {
             return null
         }
         return checkIn
     }
-    async save(checkIn:CheckIn){
-        const checkInIndex = this.items.findIndex(item=>item.id===checkIn.id)
-        if(checkInIndex>=0){
+    async save(checkIn: CheckIn) {
+        const checkInIndex = this.items.findIndex(item => item.id === checkIn.id)
+        if (checkInIndex >= 0) {
             this.items[checkInIndex] = checkIn
-            
+
         }
         return checkIn
     }
-  
-    items:CheckIn[] = [ ]
+
+    items: CheckIn[] = []
 
     async countByUserId(userId: string) {
         return this.items
-        .filter(item=>item.user_id===userId).length
+            .filter(item => item.user_id === userId).length
     }
-    
 
-    async findManyByUserId(userId: string, page: number){
+
+    async findManyByUserId(userId: string, page: number) {
 
         return this.items
-        .filter(item=>item.user_id===userId)
-        .slice((page-1)*20,page*20)
+            .filter(item => item.user_id === userId)
+            .slice((page - 1) * 20, page * 20)
     }
-    
+
     async findByUserIdOnDate(userId: string, date: Date) {
         const startOfTheDay = dayjs(date).startOf("date")
         const endOfTheDay = dayjs(date).endOf("date")
 
-        const checkInOnSameDate = this.items.find(checkIn=>{
-        const checkInDate = dayjs(checkIn.created_at)
-        const inSameDate = checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+        const checkInOnSameDate = this.items.find(checkIn => {
+            const checkInDate = dayjs(checkIn.created_at)
+            const inSameDate = checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
 
-        return checkIn.user_id==userId && inSameDate
+            return checkIn.user_id == userId && inSameDate
         })
-        
-       if(!checkInOnSameDate){
+
+        if (!checkInOnSameDate) {
             return null
         }
 
         return checkInOnSameDate
     }
-    
-    async create(data:Prisma.CheckInUncheckedCreateInput){
+
+    async create(data: Prisma.CheckInUncheckedCreateInput) {
         const check_in = {
-            id:randomUUID(),
-            user_id:data.user_id,
-            gym_id:data.gym_id,
-            created_at: data.created_at?new Date(data.created_at):new Date(),
-            validated_at: data.validated_at?new Date(data.validated_at):null,
+            id: randomUUID(),
+            user_id: data.user_id,
+            gym_id: data.gym_id,
+            created_at: data.created_at ? new Date(data.created_at) : new Date(),
+            validated_at: data.validated_at ? new Date(data.validated_at) : null,
         }
         this.items.push(check_in);
         return check_in;
--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -2,29 +2,29 @@ import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 
-export class InMemoryUsersRepository implements UsersRepository  {
-    
-    users:User[] = [ ]
+export class InMemoryUsersRepository implements UsersRepository {
 
-    async create(data:Prisma.UserCreateInput){
-        const user:User = {
-            id:'1',
-            name:data.name,
-            email:data.email,
-            password_hash:data.password_hash,
-            created_at:new Date(),
+    users: User[] = []
+
+    async create(data: Prisma.UserCreateInput) {
+        const user: User = {
+            id: '1',
+            name: data.name,
+            email: data.email,
+            password_hash: data.password_hash,
+            created_at: new Date(),
         }
         this.users.push(user);
         return user;
     }
 
     async findByEmail(email: string) {
-        const user = this.users.find(user=>user.email === email);
+        const user = this.users.find(user => user.email === email);
         return user || null;
     }
 
     async findById(id: string) {
-        const user = this.users.find(user=>user.id === id);
+        const user = this.users.find(user => user.id === id);
         return user || null;
     }
 }
