2cd158d9fd0160eec72e48d013401ed95b056c6a
Henry <franzhenry46@gmail.com>
2024-05-20 16:18:04 -0300

    feat:adicionando caso de uso de validaçaõ dentro de 20 minutos do checn in

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -3,6 +3,7 @@ import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { ValidateCheckInUseCase } from "./validate-check-in";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
 let checkInsRepository:CheckInsRepository
 let sut:ValidateCheckInUseCase
@@ -36,6 +37,19 @@ describe('testing the fetch user check ins history use case',()=>{
         await expect(()=>sut.execute({
             checkInId: 'invalid check in id',
         })).rejects.toBeInstanceOf(ResourceNotFoundError)
+    })
+
+    it('should not validate user check in after twenty minutes',async()=>{
+        vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+        const checkIn = await checkInsRepository.create({
+            gym_id: 'gym-01',
+            user_id: 'user-01',
+        })
+        const twentyOneMinutesInMs = 1000 * 60 * 21
+        vi.advanceTimersByTime(twentyOneMinutesInMs) 
+        await expect(()=> sut.execute({
+            checkInId:checkIn.id
+        })).rejects.toBeInstanceOf(LateCheckInValidationError)
     })
 
 })
